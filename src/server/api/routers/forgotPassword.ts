bfcc7284797b9c7f4d6f07fb7415976478e71be4
Aastha Ojha <aasthaojha05@gmail.com>
2025-04-19 14:54:13 +0530

    changes made

--- src/server/api/routers/forgotPassword.ts
+++ src/server/api/routers/forgotPassword.ts
@@ -15,6 +15,8 @@ export const forgotPasswordRouter = createTRPCRouter({
 
       const transporter = nodemailer.createTransport({
         service: "gmail",
+        port:465,
+        secure:true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
